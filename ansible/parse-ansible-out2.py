40a07d7bc14254786eb3c02c03b220e0b481e4e4
Christian Zufferey <christian@zufferey.com>
2020-07-14 10:35:58 +0200

    Commencé à écrire la partie push to InfluxDB, cela ne fonctionne pas encore

--- ansible/parse-ansible-out2.py
+++ ansible/parse-ansible-out2.py
@@ -5,10 +5,12 @@
 # version qui calcul la durée de chaque Task !
 # sources: https://janakiev.com/blog/python-shell-commands/
 
+import signal
 import sys
-# import os
+import os
 import datetime
-version = "parse-ansible-out2.py  zf200710.1350 "
+
+version = "parse-ansible-out2.py  zf200714.0957 "
 
 """
 génération du fichier logs:
@@ -21,8 +23,7 @@ cd /Users/zuzu/dev-vpsi/wp-ops/ansible
 curl https://$ABC_DEF:$KLM_NOP@awx-poc-vpsi.epfl.ch/api/v2/jobs/1219/stdout/?format=txt > awx_logs.txt
 
 reset
-./parse-ansible-out1.py awx_logs.txt
-
+./parse-ansible-out2.py awx_logs_2_sites_1_pods.txt
 
 Puis voir le résultat dans un browser
 http://noc-tst.idev-fsd.ml:9092/
@@ -37,8 +38,8 @@ curl -i -XPOST "$dbflux_srv_host:$dbflux_srv_port/query?u=$dbflux_u_admin&p=$dbf
 
 
 # True False
-zdebug1 = True
-zdebug2 = False
+zverbose_v = True
+zverbose_vv = False
 zprint_curl = True
 zsend_grafana = False
 
@@ -60,20 +61,27 @@ ztask_time_obj_2 = ""
 ztask_unix_time_2 = 0
 
 
+def signal_handler(signal, frame):
+    print("oups il y a eu un CTRL-C !")
+    quit()
+    # sys.exit(0)
+  
 def zget_unix_time(zdate):
     #    date_time_obj = datetime.datetime.strptime(zdate, '%Y-%m-%d %H:%M:%S.%f')
     zdate_time_obj = zdate
-    if zdebug2: print("zdate_time_obj: [" + str(zdate_time_obj) + "]")
+    if zverbose_vv: print("zdate_time_obj: [" + str(zdate_time_obj) + "]")
     zdate_time_1970_obj = datetime.datetime.strptime(
         "1970-01-01 01:00:00", '%Y-%m-%d %H:%M:%S')  # Astuce pour faire UTC-1 à cause de Grafana !
-    if zdebug2: print("zdate_time_1970_obj: [" + str(zdate_time_1970_obj) + "]")
+    if zverbose_vv: print("zdate_time_1970_obj: [" + str(zdate_time_1970_obj) + "]")
     zdate_time_unix_obj = (zdate_time_obj - zdate_time_1970_obj)
-    if zdebug2: print("zdate_time_unix_obj: [" + str(zdate_time_unix_obj) + "]")
+    if zverbose_vv: print("zdate_time_unix_obj: [" + str(zdate_time_unix_obj) + "]")
     return zdate_time_unix_obj
 
 
 if (__name__ == "__main__"):
     print("\n" + version + "\n")
+
+    signal.signal(signal.SIGINT, signal_handler)
 
     if len(sys.argv) == 1:
         print("Usage: ./parse-ansible-out3.py fichier_log_a_parser\n\n")
@@ -116,19 +124,19 @@ if (__name__ == "__main__"):
 
             # Est-ce la Task 'debug' ?
             if zline.find(": debug") != -1:
-                if zdebug2: print(str(i) + " " + zline)
+                if zverbose_vv: print(str(i) + " " + zline)
                 ztask_number = ztask_number + 1
                 db_logs[ztask_number] = {"zsite_name": {}}
 
                 # On passe à la ligne suivante
                 zline = zfile.readline()
                 i = i + 1
-                if zdebug2: print(str(i) + " " + zline)
+                if zverbose_vv: print(str(i) + " " + zline)
 
                 # Récupération du path de la Task
                 zpath = "/project/ansible/roles/wordpress-instance/tasks/"
                 ztask_path = zline[zline.find(zpath) + len(zpath):-1]
-                if zdebug2: print(str(i) + " ztask_path: [" + ztask_path + "]")
+                if zverbose_vv: print(str(i) + " ztask_path: [" + ztask_path + "]")
                 db_logs[ztask_number].update({"ztask_path": ztask_path})
 
                 while True:
@@ -139,24 +147,24 @@ if (__name__ == "__main__"):
                     # On passe à la ligne suivante
                     zline = zfile.readline()
                     i = i + 1
-                    if zdebug2: print(str(i) + " " + zline)
+                    if zverbose_vv: print(str(i) + " " + zline)
 
                     # Est-ce une ligne ok: [site name] ?
                     if zline.find("ok: [") != -1:
 
                         # Récupération du nom du site
                         ztask_site = zline[1 + zline.find("["):zline.find("]")]
-                        if zdebug2: print(str(i) + " ztask_site: [" + ztask_site + "]")
+                        if zverbose_vv: print(str(i) + " ztask_site: [" + ztask_site + "]")
                         db_logs[ztask_number]["zsite_name"].update({ztask_site: {}})
 
                         # On passe à la ligne suivante
                         zline = zfile.readline()
                         i = i + 1
-                        if zdebug2: print(str(i) + " " + zline)
+                        if zverbose_vv: print(str(i) + " " + zline)
 
                         # Est-ce la ligne 'ztime' ?
                         if zline.find("ztime, ") != -1:
-                            if zdebug2: print("c'est une ligne ztime............")
+                            if zverbose_vv: print("c'est une ligne ztime............")
 
                             # Récupération de la position dans le logs
                             ztask_line = i
@@ -164,28 +172,28 @@ if (__name__ == "__main__"):
 
                             # Récupération du nom de la Task
                             ztask_name = zline[1 + zline.find("/"):zline.find("/", 1 + zline.find("/"))]
-                            if zdebug2: print(str(i) + " ztask_name: [" + ztask_name + "]")
+                            if zverbose_vv: print(str(i) + " ztask_name: [" + ztask_name + "]")
                             db_logs[ztask_number].update({"ztask_name": ztask_name})
 
                             # Récupération du time de la Task
                             ztask_time = zline[2 + zline.find("/ "):-2]
-                            if zdebug2: print(str(i) + " ztask_time: [" + ztask_time + "]")
+                            if zverbose_vv: print(str(i) + " ztask_time: [" + ztask_time + "]")
                             db_logs[ztask_number]["zsite_name"][ztask_site].update({"ztask_time": ztask_time})
 
-                            if zdebug2: print("..................................................")
+                            if zverbose_vv: print("..................................................")
                             # print(str(db_logs[ztask_number]))
-                            if zdebug2: print("Task number: " + str(ztask_number))
-                            if zdebug2: print("Task line: " + str(db_logs[ztask_number]["zsite_name"][ztask_site]["ztask_line"]))
-                            if zdebug2: print("Task name: " + db_logs[ztask_number]["ztask_name"])
-                            if zdebug2: print("Path path: " + db_logs[ztask_number]["ztask_path"])
-                            if zdebug2: print("Site name: " + ztask_site)
-                            if zdebug2: print("Timestamp: " + db_logs[ztask_number]["zsite_name"][ztask_site]["ztask_time"])
-                            if zdebug2: print("..................................................")
+                            if zverbose_vv: print("Task number: " + str(ztask_number))
+                            if zverbose_vv: print("Task line: " + str(db_logs[ztask_number]["zsite_name"][ztask_site]["ztask_line"]))
+                            if zverbose_vv: print("Task name: " + db_logs[ztask_number]["ztask_name"])
+                            if zverbose_vv: print("Path path: " + db_logs[ztask_number]["ztask_path"])
+                            if zverbose_vv: print("Site name: " + ztask_site)
+                            if zverbose_vv: print("Timestamp: " + db_logs[ztask_number]["zsite_name"][ztask_site]["ztask_time"])
+                            if zverbose_vv: print("..................................................")
 
                             # On passe à la ligne suivante
                             zline = zfile.readline()
                             i = i + 1
-                            if zdebug2: print(str(i) + " " + zline)
+                            if zverbose_vv: print(str(i) + " " + zline)
                         else:
                             # Ce n'est pas une task debug avec un ztime, on recule donc le pointeur du dictionnaire
                             ztask_number = ztask_number - 1
@@ -219,34 +227,63 @@ if (__name__ == "__main__"):
 
     # Calcul les durations pour chaque sites
     for i in range(1, ztask_number):
-        if zdebug2: print(str(i))
+        if zverbose_vv: print(str(i))
 
         for j in db_logs[i]["zsite_name"]:
-            if zdebug2: print(j + ": " + db_logs[i]["zsite_name"][j]["ztask_time"])
+            if zverbose_vv: print(j + ": " + db_logs[i]["zsite_name"][j]["ztask_time"])
 
             ztask_time_1 = db_logs[i]["zsite_name"][j]["ztask_time"][0:-3]
             ztask_time_obj_1 = datetime.datetime.strptime(ztask_time_1, '%Y-%m-%d %H:%M:%S.%f')
             ztask_unix_time_1 = zget_unix_time(ztask_time_obj_1).total_seconds()
-            if zdebug2: print("ztask_unix_time_1: " + str(ztask_unix_time_1))
+            if zverbose_vv: print("ztask_unix_time_1: " + str(ztask_unix_time_1))
 
             ztask_time_2 = db_logs[i + 1]["zsite_name"][j]["ztask_time"][0:-3]
             ztask_time_obj_2 = datetime.datetime.strptime(ztask_time_2, '%Y-%m-%d %H:%M:%S.%f')
             ztask_unix_time_2 = zget_unix_time(ztask_time_obj_2).total_seconds()
-            if zdebug2: print("ztask_unix_time_2: " + str(ztask_unix_time_2))
+            if zverbose_vv: print("ztask_unix_time_2: " + str(ztask_unix_time_2))
 
             ztask_duration = ztask_unix_time_2 - ztask_unix_time_1
-            if zdebug2: print("Durée: " + str(ztask_duration))
+            if zverbose_vv: print("Durée: " + str(ztask_duration))
             
             db_logs[i]["zsite_name"][j].update({"ztask_duration": ztask_duration})
 
-            if zdebug1: print("..................................................")
+            if zverbose_v: print("..................................................")
             # print(str(db_logs[ztask_number]))
-            if zdebug1: print("Task number: " + str(i))
-            if zdebug1: print("Task line: " + str(db_logs[i]["zsite_name"][j]["ztask_line"]))
-            if zdebug1: print("Task name: " + db_logs[i]["ztask_name"])
-            if zdebug1: print("Path path: " + db_logs[i]["ztask_path"])
-            if zdebug1: print("Site name: " + j)
-            if zdebug1: print("Timestamp: " + db_logs[i]["zsite_name"][j]["ztask_time"])
-            if zdebug1: print("Duration: " + str(db_logs[i]["zsite_name"][j]["ztask_duration"]))
-            if zdebug1: print("..................................................")
+            if zverbose_v: print("Task number: " + str(i))
+            if zverbose_v: print("Task line: " + str(db_logs[i]["zsite_name"][j]["ztask_line"]))
+            if zverbose_v: print("Task name: " + db_logs[i]["ztask_name"])
+            if zverbose_v: print("Path path: " + db_logs[i]["ztask_path"])
+            if zverbose_v: print("Site name: " + j)
+            if zverbose_v: print("Timestamp: " + db_logs[i]["zsite_name"][j]["ztask_time"])
+            if zverbose_v: print("Duration: " + str(db_logs[i]["zsite_name"][j]["ztask_duration"]))
+            if zverbose_v: print("..................................................")
+
+            
+            ztable = "awx_logs1"
+            ztask_name_1 = db_logs[i]["ztask_name"]
+            ztask_line_1 = db_logs[i]["zsite_name"][j]["ztask_line"]
+            ztask_path_1 = db_logs[i]["ztask_path"]
+            
+            ztask_name = ztask_name_1.replace(" ", "_") + "_" + str(ztask_line_1)
+            ztask_path = ztask_path_1.replace(" ", "_")
+            ztask_path = ztask_path.replace(":", "_")
+            ztask_path = ztask_path.replace(".", "_")
+            # 
+            ztask_unix_time_nano = db_logs[i]["zsite_name"][j]["ztask_time"]*1000000000
+
+            # zdate_time_obj_1900 = datetime.datetime.strptime("1900-01-01 00:00:00", '%Y-%m-%d %H:%M:%S')
+            # ztask_duration = db_logs[i]["zsite_name"][j]["ztask_duration"]
+
+            # zcmd = 'curl -i -XPOST "$dbflux_srv_host:$dbflux_srv_port/write?db=$dbflux_db&u=$dbflux_u_user&p=$dbflux_p_user"  --data-binary "' + ztable + ',action=' + ztask_path + ',task=' + ztask_name + ' duration=' + str(ztask_duration) + ' ' + '%0.0f' % (ztask_unix_time_nano) + '"'
+            # if zprint_curl: print(zcmd)
+            # 
+            # if zsend_grafana:
+            #     zerr = os.system(zcmd)
+            #     if zerr != 0:
+            #         print(zerr)
+
+
+
+
+
 
